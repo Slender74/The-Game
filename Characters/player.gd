b8870c9317f38d0bf04ccf005d716b569f173945
Erick Sierra Serrano <slenderjaguar75@gmail.com>
2024-02-20 00:03:43 -0600

    Added Dashing

--- Characters/player.gd
+++ Characters/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var speed : float = 250.0
 @export var jump_velocity : float = -200.0
 @export var double_jump_velocity : float = -150.0
+@export var dash_velocity : float = 900.0
+
+
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -13,6 +16,8 @@ var has_double_jumped : bool = false
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
 var was_in_air : bool = false
+var dashing : bool = false
+var can_dash : bool = true
 
 
 
@@ -37,14 +42,29 @@ func _physics_process(delta: float) -> void:
 			#double jump in air
 			velocity.y = double_jump_velocity
 			double_jump()
-			
+	
+	
+	
+	
+	if Input.is_action_just_pressed("dash") && can_dash:
+		dashing = true
+		can_dash = false
+		$dash_timer.start()
+		$can_dash_timer.start()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_vector("left", "right", "up", "down")
 	if direction.x != 0 && animated_sprite.animation != "jump_end":
 		velocity.x = direction.x * speed
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	if dashing:
+		velocity.x = direction.x * dash_velocity
+	else:
+		velocity.x = move_toward(velocity.x, 0, dash_velocity)
+	
 	
 	move_and_slide()
 	update_animation()
@@ -84,3 +104,12 @@ func land():
 func _on_animated_sprite_2d_animation_finished():
 	if(["jump_end", "jump_start", "jump_double"].has(animated_sprite.animation)):
 		animation_locked = false 
+
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+
+func _on_can_dash_timer_timeout() -> void:
+	can_dash = true
+	
